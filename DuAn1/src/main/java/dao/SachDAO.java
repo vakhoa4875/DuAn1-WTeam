9d2060bf2c6ca1c5ff020c9e48a4c25243f4cd45
khoa <128579628+vakhoa4875@users.noreply.github.com>
2023-11-18 20:13:28 +0700

    abc

--- DuAn1/src/main/java/dao/SachDAO.java
+++ DuAn1/src/main/java/dao/SachDAO.java
@@ -2,15 +2,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package dao;
 
+import library.Jdbc;
+import model.Sach;
+
 /**
- * 
+ *
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class SachDAO {
-    public void insert() {
-        
+
+    public void insert(Sach sach) {
+        String insertQuery = """
+                             INSERT INTO SACH (IDSACH, TENSACH, GIANIEMYET, THUMBNAIL, AVATAR, MOTA, DANHGIATB, MINAGE, PDFAVAI, AUDIOAVAI, SOTRANG, NGONNGU, SOURCEPDF, sourceSound, FREE, LIKECOUNT, viewcount)
+                             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""";
+        Jdbc.executeUpdate(insertQuery, 
+                sach.getIdSach(),
+                sach.getTenSach(),
+                sach.getGiaNiemYet(),
+                sach.getThumbnail(),
+                sach.getAvatar(),
+                sach.getMoTa(),
+                sach.getDanhGiaTB(),
+                sach.getMinAge(),
+                sach.getPdfAvai() == true ? 1 : 0,
+                sach.getAudioAvai() == true ? 1 : 0,
+                sach.getSoTrang(),
+                sach.getNgonNgu(),
+                sach.getSourcePDF(),
+                sach.getSourceSound(),
+                sach.getFree() == true ? 1 : 0,
+                sach.getLikeCount(),
+                sach.getViewCount());
     }
 }
