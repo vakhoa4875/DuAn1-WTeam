e5c2145019c0e1d99dfef79b6af138274bffd94f
khoa <128579628+vakhoa4875@users.noreply.github.com>
2023-12-03 21:24:08 +0700

    abc

--- DuAn1/src/main/java/dao/NoiBoDAO.java
+++ DuAn1/src/main/java/dao/NoiBoDAO.java
@@ -40,7 +40,7 @@ public class NoiBoDAO {
     }
 
     public void update(NoiBo noiBo) {
-        String updateQuery = "update noiBo set idPB = ?, idQuanLy = ?, luong = ?, fullTime = ?, ngayThue = ?, caLam = ?, quanLy = ?, luongbong = ?, ngaySinh = ?, avatar = ?, gioiTinh = ?, hoTen = ?  where userid = ?";
+        String updateQuery = "update noiBo set idPB = ?, idQuanLy = ?, luong = ?, fullTime = ?, ngayThue = ?, caLam = ?, quanLy = ?, luongbong = ?, ngaySinh = ?, avatar = ?, gioiTinh = ?  where userid = ?";
         Jdbc.executeUpdate(updateQuery,
                 noiBo.getIdPB(),
                 noiBo.getIdQuanLy(),
@@ -53,7 +53,6 @@ public class NoiBoDAO {
                 noiBo.getNgaySinh(),
                 noiBo.getAvatar(),
                 noiBo.getGioiTinh(),
-                noiBo.getHoTen(),
                 noiBo.getUserID()
         );
     }
