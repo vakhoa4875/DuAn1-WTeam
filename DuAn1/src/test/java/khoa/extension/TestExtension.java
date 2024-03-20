b3db28a96f3a7d25e39c5a723cbc7b4d156c0807
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-03-21 00:09:28 +0700

    commit

---
+++ DuAn1/src/test/java/khoa/extension/TestExtension.java
@@ -0,0 +1,14 @@
+package khoa.extension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestExtension {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
